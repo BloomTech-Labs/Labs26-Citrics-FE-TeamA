585644beea881ba230a43e6044851c7bff03c501
Toricruz Mendiola <mtoricruz@gmail.com>
2020-09-09 09:48:27 -0700

    cleaned up test files. removed tests that test okta and removed files for components we do not use.

--- src/__tests__/ExampleListContainer.test.js
+++ src/__tests__/ExampleListContainer.test.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { render, act, cleanup, waitForElement } from '@testing-library/react';
+import { render, act, cleanup } from '@testing-library/react';
 
 import ExampleListContainer from '../components/pages/ExampleList/ExampleListContainer';
 
-afterEach(() => {
-  cleanup();
-  jest.clearAllMocks();
-});
-
-jest.mock('../api', () => ({
-  getExampleData: jest.fn(() => Promise.resolve([])),
-}));
+afterEach(cleanup);
 
 describe('<ExampleListContainer /> test suite', () => {
   test('container renders without crashing', async () => {
--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { render, cleanup, wait, waitFor } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { HomePage } from '../components/pages/Home';
 import { LoadingComponent } from '../components/common';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 afterEach(cleanup);
 
-jest.mock('@okta/okta-react', () => ({
-  useOktaAuth: () => {
-    return {
-      authState: {
-        isAuthenticated: true,
-      },
-      authService: {
-        getUser: () => Promise.resolve({ name: 'sara' }),
-      },
-    };
-  },
-}));
-
 describe('<HomeContainer /> testing suite', () => {
   test('mounts a page', async () => {
-    const { findByText, getByText, queryByText } = render(
+    const { getByText } = render(
       <Router>
         <HomePage
           LoadingComponent={() => (
@@ -30,13 +17,5 @@ describe('<HomeContainer /> testing suite', () => {
         />
       </Router>
     );
-    let loader = getByText(/...fetching profile/i);
-    expect(loader).toBeInTheDocument();
-
-    await waitFor(async () => {
-      await findByText(/hi sara/i);
-    });
-    loader = queryByText(/...fetching profile/i);
-    expect(loader).toBeNull();
   });
 });
