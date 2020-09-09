f3c882f757218090653cc447ab6fb558e8a124ad
Toricruz Mendiola <mtoricruz@gmail.com>
2020-09-09 10:08:25 -0700

    removed code that caused warnings

--- src/__tests__/RenderHomePage.test.js
+++ src/__tests__/RenderHomePage.test.js
@@ -2,11 +2,10 @@ import RenderHomePage from '../components/pages/Home/RenderHomePage';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 describe('<RenderHomePage /> test suite', () => {
   test('it handles a loading state', () => {
-    const { getByText } = render(
+    render(
       <Router>
         <RenderHomePage />
       </Router>
