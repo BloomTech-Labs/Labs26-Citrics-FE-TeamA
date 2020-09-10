73620f9e8cdaab9df4a1d177014536cfb9442e76
Michael Perez <mperez6473@gmail.com>
2020-09-10 13:17:49 -0500

    Added a test to do when the column adding logic is completed.

--- src/__tests__/Add_City_Button.test.js
+++ src/__tests__/Add_City_Button.test.js
@@ -2,7 +2,11 @@ import React from 'react';
 import AddCityButton from '../components/common/AddCityButton';
 import { render } from '@testing-library/react';
 
+const { getByText } = render(<AddCityButton />);
 test('AddCityButton renders', () => {
-  const { getByText } = render(<AddCityButton />);
   expect(getByText(/Add City/i)).toBeInTheDocument();
+});
+
+test('AddCityButton adds a new column', () => {
+  // TODO ##########
 });
