ef28c96042e46dd3fd9ee08d0e82eca00ed3eb0c
Toricruz Mendiola <mtoricruz@gmail.com>
2020-09-09 19:37:11 -0700

    Linked navigation links to respective pages. Placeholder links included for future implementation of paths

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,32 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Router } from 'react-router-dom';
 
 import { Layout, Menu } from 'antd';
+import './styles/NavBar.scss';
 
 const { Header } = Layout;
-
-// import './NavBar.scss';
 
 function NavBar() {
   return (
     <Layout className="layout">
       <Header>
         <Menu theme="dark" mode="horizontal">
-          <Menu.Item key="1">
-            <Link exact path="/">
-              Home
-            </Link>
+          <Menu.Item key="1" className="link">
+            <Link to="/">Home</Link>
           </Menu.Item>
-          <Menu.Item key="2">
-            <Link exact path="/about-us">
-              About Us
-            </Link>
+          <Menu.Item key="2" className="link">
+            <Link to="/about-us">About Us</Link>
           </Menu.Item>
-          <Menu.Item key="3">
-            <Link exact path="/why-citrics">
-              Why Citrics?
-            </Link>
+          <Menu.Item key="3" className="link">
+            <Link to="/why-citrics"> Why Citrics?</Link>
           </Menu.Item>
+          <h1 className="title" selectable="false">
+            Citrics
+          </h1>
         </Menu>
       </Header>
     </Layout>
