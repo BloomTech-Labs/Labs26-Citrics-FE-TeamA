8b6f0f758b8569d130aec408297cf101aa16cf8d
Michael Perez <mperez6473@gmail.com>
2020-09-10 16:23:50 -0500

    Added Search bar to home and added test for search.

--- src/__tests__/AddCityBar.test.js
+++ src/__tests__/AddCityBar.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import AddCityBar from '../components/common/AddCityBar';
-import { render } from '@testing-library/react';
-import AddCityButton from '../components/common/AddCityButton';
+import { render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 const { getByPlaceholderText } = render(<AddCityBar />);
 test('AddCityBar should have placeholder text', () => {
   expect(getByPlaceholderText(/input search/i)).toBeInTheDocument();
+});
+test('Should add text to the input', async () => {
+  const { getByPlaceholderText } = render(<AddCityBar />);
+  fireEvent.click(getByPlaceholderText(/input/i));
+  await userEvent.type(getByPlaceholderText(/input/i), 'Hello there!');
+  //   fireEvent.keyDown(getByPlaceholderText(/input/i), { key: 'A', code: 'KeyA' });
+  //   fireEvent.keyDown(getByPlaceholderText(/input/i), {
+  //     key: 'Enter',
+  //     code: 'Enter',
+  //   });
+  expect(getByPlaceholderText(/input/i)).toHaveValue('Hello there!');
 });
