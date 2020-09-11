bfb8089d8930344f9ba1aaaf16c82641fd4364f6
Michael Perez <mperez6473@gmail.com>
2020-09-10 20:01:36 -0500

    Added some code that adds window.matchMedia.

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,8 +1,19 @@
+// Added as JSDOM does not support window.matchMedia yet
+// Needs to be above imports
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { HomePage } from '../components/pages/Home';
 import { LoadingComponent } from '../components/common';
 import { BrowserRouter as Router } from 'react-router-dom';
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {},
+    };
+  };
 
 afterEach(cleanup);
 
@@ -10,11 +21,7 @@ describe('<HomeContainer /> testing suite', () => {
   test('mounts a page', async () => {
     render(
       <Router>
-        <HomePage
-          LoadingComponent={() => (
-            <LoadingComponent message="...fetching profile" />
-          )}
-        />
+        <HomePage />
       </Router>
     );
   });
--- src/__tests__/RenderHomePage.test.js
+++ src/__tests__/RenderHomePage.test.js
@@ -1,8 +1,18 @@
-import './matchMedia.mock.js';
+// Added as JSDOM does not support window.matchMedia yet
+// Needs to be above imports
 import RenderHomePage from '../components/pages/Home/RenderHomePage';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {},
+    };
+  };
 
 describe('<RenderHomePage /> test suite', () => {
   test('it handles a loading state', () => {
