7098f39262565553bd3b6c47f0ca39dbd5375e42
Michael Perez <mperez6473@gmail.com>
2020-09-10 20:31:29 -0500

    Made RenderHomePage consist of just components

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import AddCityButton from '../../common/AddCityButton';
-import AddCityBar from '../../common/AddCityBar';
 import CityReport from '../../common/CityReport';
-
+import Title from '../../common/Title';
+import AddingCities from '../../common/AddingCities';
 function RenderHomePage() {
   return (
-    <div>
-      <h1>Welcome to Labs Basic SPA</h1>
-      <section className="addingCities">
-        <AddCityBar />
-        {!AddCityBar && <AddCityButton />}
-        {/* Adjust to have the button show when the city is recieved from DS */}
-      </section>
-      <div>
-        <CityReport />
-      </div>
-    </div>
+    <>
+      <Title />
+      <AddingCities />
+      <CityReport />
+    </>
   );
 }
 export default RenderHomePage;
