0fba94ca0233e2205ab00bdd7f9575d33770a2e4
Michael Perez <mperez6473@gmail.com>
2020-09-10 20:30:51 -0500

    Main title and subtitle area moved to their own component

---
+++ src/components/common/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Title() {
+  return (
+    <section>
+      <h1>Critics</h1>
+      <h2>Find and Compare City Data</h2>
+    </section>
+  );
+}
