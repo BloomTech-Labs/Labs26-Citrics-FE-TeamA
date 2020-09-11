e78b4c4a8d7d1beaeeddab6ba7c875d9780a8b88
Michael Perez <mperez6473@gmail.com>
2020-09-10 20:35:20 -0500

    Added styling to title section

--- src/components/common/Title.js
+++ src/components/common/Title.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styles/Title.scss';
 
 export default function Title() {
   return (
-    <section>
-      <h1>Critics</h1>
+    <section className="TitleSection">
+      <h1>Citics</h1>
       <h2>Find and Compare City Data</h2>
     </section>
   );
