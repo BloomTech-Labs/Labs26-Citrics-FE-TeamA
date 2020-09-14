62fbfc2581e95e600d7e2e15081935190a7f45f4
Michael Perez <mperez6473@gmail.com>
2020-09-14 15:45:42 -0500

    Autocomplete is finding the first letter of cities.

--- src/components/common/AutoComplete.jsx
+++ src/components/common/AutoComplete.jsx
@@ -2,12 +2,35 @@ import React, { useState } from 'react';
 import cityData from '../../api/cityData';
 
 console.log(cityData);
+const cityDataArr = {};
+cityData.forEach(value => {
+  // console.log(value.city[0], value.state);
+  if (value.city in cityDataArr) {
+    cityDataArr[value.city].push([value.city, value.state]);
+  } else {
+    cityDataArr[value.city] = [];
+    cityDataArr[value.city].push([value.city, value.state]);
+  }
+});
+console.log(cityDataArr);
+
 function AutoCompleteInput() {
   const [city, setCity] = useState({ city: '' });
 
   const handleNameChange = event => {
     setCity({ city: event.target.value });
     console.log(event.target.value);
+
+    Object.keys(cityDataArr).forEach(value => {
+      // console.log(value[0]);
+      if (event.target.value === value[0]) {
+        console.log(value);
+        console.log(cityDataArr[value]);
+      }
+    });
+    // if (event.target.value == cityDataArr[]) {
+    //   console.log(value.city);
+    // }
   };
 
   const handleSubmit = event => {
