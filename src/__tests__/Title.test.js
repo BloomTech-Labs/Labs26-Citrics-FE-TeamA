a046d10141143d74fd7dcc98ac3903561e3b36fb
Michael Perez <mperez6473@gmail.com>
2020-09-15 18:12:06 -0500

    Change of title text test

--- src/__tests__/Title.test.js
+++ src/__tests__/Title.test.js
@@ -4,6 +4,7 @@ import Title from '../components/common/Title';
 
 const { getByText } = render(<Title />);
 test('Main title section should display', () => {
-  expect(getByText(/Citrics/i)).toBeInTheDocument();
-  expect(getByText(/Compare City Data/i)).toBeInTheDocument();
+  expect(
+    getByText(/Make the right choice for your new home/i)
+  ).toBeInTheDocument();
 });
