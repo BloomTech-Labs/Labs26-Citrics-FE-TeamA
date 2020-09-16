7f9ddd305762d1c6140c9de49b5e1acf549db66f
Toricruz Mendiola <mtoricruz@gmail.com>
2020-09-16 10:55:51 -0700

    added ReportContext to handle data from DS API. This will allow dynamic city data on the report. Wrapped provider around where data visualizations go in the report, and will discuss where else we may add the provider later in the day.

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -3,6 +3,7 @@ import CityReport from '../../common/CityReport';
 import Title from '../../common/Title';
 import AddingCities from '../../common/AddingCities';
 import StaticHomePage from '../../common/StaticHomePage';
+
 function RenderHomePage() {
   // state of home page. default state is StaticHomePageComp
   return (
