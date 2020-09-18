11724fad883ab2a58eb9e6da159bd0fc8ca2b32c
Toricruz Mendiola <mtoricruz@gmail.com>
2020-09-18 13:41:20 -0700

    implemented refactor comparelist state for cities. combined endpoint calls in one file, being reportData. implemented separate componenent states for different endpoints; weather/walkcity/rent endpoints

--- src/api/reportData.js
+++ src/api/reportData.js
@@ -4,7 +4,7 @@ export function reportWalkData(city, state) {
   return axios
     .get(`https://ds.citrics.dev/walkability/${city}_${state}`)
     .then(res => {
-      console.log('WALK ENDPOINT DATA', res);
+      // console.log('WALK ENDPOINT DATA', res.data);
       return JSON.parse(res.data);
     })
     .catch(err => {
@@ -17,11 +17,24 @@ export function reportWeatherData(city, state) {
   return axios
     .get(`https://ds.citrics.dev/current/${city}_${state}`)
     .then(res => {
-      console.log('WEATHER ENDPOINT DATA', res);
+      // console.log('WEATHER ENDPOINT DATA', res.data);
       return JSON.parse(res.data);
     })
     .catch(err => {
       console.log('WEATHER ERR', err);
+      return err;
+    });
+}
+
+export function reportRentData(city, state) {
+  return axios
+    .get(`https://ds.citrics.dev/rent_viz/${city}_${state}`)
+    .then(response => {
+      // console.log('RENT ENDPOINT DATA', response.data);
+      return JSON.parse(response.data);
+    })
+    .catch(err => {
+      console.log(err);
       return err;
     });
 }
--- src/components/common/Plotly.js
+++ src/components/common/Plotly.js
@@ -1,33 +1,83 @@
 import React, { useEffect, useState, useContext } from 'react';
 import Plot from 'react-plotly.js';
-import { fetchCityPlot } from '../../api/plotly';
+import {
+  reportRentData,
+  reportWeatherData,
+  reportWalkData,
+} from '../../api/reportData';
 import { ReportContext } from '../../state/contexts/ReportContext';
+
+import { Popover, Button } from 'antd';
 
 export default function Plotly() {
   //  State for plotly json info
   const [thisCityData, setThisCityData] = useState([]);
+  const [walkCityData, setwalkCityData] = useState([]);
+  const [weatherCityData, setweatherCityData] = useState([]);
   let { compareList } = useContext(ReportContext);
+  let content = (
+    <div>
+      <p>
+        {walkCityData.city} Score: {walkCityData.walkability}
+      </p>
+      <p>How fun it is to walk somewhere yenno?</p>
+    </div>
+  );
+  let content2 = (
+    <div>
+      <p> </p>
+      <p>How fun it is to walk somewhere yenno?</p>
+    </div>
+  );
   // retrieves the data from DS API and sets to state;
   useEffect(() => {
-    fetchCityPlot(compareList.city1)
+    reportRentData(compareList.city1, compareList.state1)
       .then(res => {
-        console.log('fetchCityPlot RES', res.data);
+        console.log('reportRentData RES', res.data);
         setThisCityData(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    reportWeatherData(compareList.city1, compareList.state1)
+      .then(res => {
+        console.log('reportWeatherData RES', res);
+        setweatherCityData(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    reportWalkData(compareList.city1, compareList.state1)
+      .then(res => {
+        console.log('reportWalkData RES', res.data);
+        setwalkCityData(res);
       })
       .catch(err => {
         console.log(err);
       });
   }, [compareList]);
 
-  let thisCityDataArr = [];
-  thisCityData &&
-    Object.values(thisCityData).forEach(value => {
-      //   console.log(value);
-      thisCityDataArr.push(value);
-    });
+  // let thisCityDataArr = [];
+  // console.log('thisCityDataArr -->', thisCityDataArr);
+  // thisCityData &&
+  //   Object.values(thisCityData).forEach(value => {
+  //     // console.log('thisCityDataArr Value',value);
+  //     thisCityDataArr.push(value);
+  //   });
+  console.log('WALK CITY DATA', walkCityData);
   return (
     thisCityData && (
-      <Plot data={thisCityDataArr[0]} layout={thisCityDataArr[1]} />
+      <div>
+        <Plot data={thisCityData.data} layout={thisCityData.layout} />
+        <Popover content={content} title="Walkability Score">
+          <Button type="primary">Walkability</Button>
+        </Popover>
+        <Popover content={content2} title="Weather">
+          <Button type="primary">Weather</Button>
+        </Popover>
+      </div>
     )
   );
 }
