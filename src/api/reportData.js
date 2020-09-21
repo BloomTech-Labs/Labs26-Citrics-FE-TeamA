bca574ad8162517607edfcbd54787b9c15b0d44b
Michael Perez <mperez6473@gmail.com>
2020-09-21 17:18:49 -0500

    axios call to unemployment data viz

--- src/api/reportData.js
+++ src/api/reportData.js
@@ -4,11 +4,9 @@ export function reportWalkData(city, state) {
   return axios
     .get(`https://ds.citrics.dev/walkability/${city}_${state}`)
     .then(res => {
-      // console.log('WALK ENDPOINT DATA', res.data);
       return JSON.parse(res.data);
     })
     .catch(err => {
-      console.log('WALK ERR', err);
       return err;
     });
 }
@@ -17,11 +15,9 @@ export function reportWeatherData(city, state) {
   return axios
     .get(`https://ds.citrics.dev/current/${city}_${state}`)
     .then(res => {
-      // console.log('WEATHER ENDPOINT DATA', res.data);
       return JSON.parse(res.data);
     })
     .catch(err => {
-      console.log('WEATHER ERR', err);
       return err;
     });
 }
@@ -30,11 +26,20 @@ export function reportRentData(city, state) {
   return axios
     .get(`https://ds.citrics.dev/rent_viz/${city}_${state}`)
     .then(response => {
-      // console.log('RENT ENDPOINT DATA', response.data);
       return JSON.parse(response.data);
     })
     .catch(err => {
-      console.log(err);
+      return err;
+    });
+}
+export function unemploymentData(state) {
+  return axios
+    .get(`https://ds.citrics.dev/viz/${state}`)
+    .then(response => {
+      console.log(JSON.parse(response.data));
+      return JSON.parse(response.data);
+    })
+    .catch(err => {
       return err;
     });
 }
