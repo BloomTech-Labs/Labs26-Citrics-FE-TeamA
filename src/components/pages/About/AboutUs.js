b63fb34d07cec3d97aaa7d7c20c61311896b2e5d
Toricruz Mendiola <mtoricruz@gmail.com>
2020-09-21 15:14:23 -0700

    pushing up most recent additions of about page. formatted grid correctly/added github and linkedin links for every teammate and implemented all bios except one. default pics remain until post stakeholder meeting.

--- src/components/pages/About/AboutUs.js
+++ src/components/pages/About/AboutUs.js
@@ -27,15 +27,29 @@ export default function AboutUs() {
                     src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
                   />
                 </div>
-                <h3>FName LName</h3>
-                <p className="member-role">Member Role</p>
-                <div className="member-social">Social Links</div>
+                <h3>Vladislav Mogilevskiy</h3>
+                <p className="member-role">Associate Project Lead</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/vladmog"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/vladmog/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
+                </div>
                 <p className="member-bio">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.
+                  Vladislav Mogilevskiy is a full stack web developer,
+                  cross-functional Agile-development tech lead, and skilled
+                  steel fabricator. I build web applications using React +
+                  Express + Node.js as my primary stack.
                 </p>
               </div>
             </div>
@@ -48,63 +62,35 @@ export default function AboutUs() {
                     src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
                   />
                 </div>
-                <h3>FName LName</h3>
-                <p className="member-role">Member Role</p>
-                <div className="member-social">Social Links</div>
-                <p className="member-bio">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <div className="member-info">
-                <div className="member-pic">
-                  <img
-                    className="member-photo"
-                    alt="default pic"
-                    src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
-                  />
+                <h3>Sean Antosiak</h3>
+                <p className="member-role">Technical Project Lead</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/Seanantosiak"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/Seanantosiak"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
                 </div>
-                <h3>FName LName</h3>
-                <p className="member-role">Member Role</p>
-                <div className="member-social">Social Links</div>
                 <p className="member-bio">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <div className="member-info">
-                <div className="member-pic">
-                  <img
-                    className="member-photo"
-                    alt="default pic"
-                    src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
-                  />
-                </div>
-                <h3>FName LName</h3>
-                <p className="member-role">Member Role</p>
-                <div className="member-social">Social Links</div>
-                <p className="member-bio">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.
+                  Sean Antosiak is a data scientist with an interest in using
+                  data wrangling and feature engineering to get as much out of a
+                  dataset as possible. The main tools he is currently working
+                  with are Python, NumPy, Pandas, and scikit-learn.
                 </p>
               </div>
             </div>
           </div>
           <div className="about-us-team-div">
-            <h2 id="managers" className="team-name">
+            <h2 id="front-end" className="team-name">
               Front-end Development
             </h2>
             <span>Team</span>
@@ -119,9 +105,141 @@ export default function AboutUs() {
                     src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
                   />
                 </div>
-                <h3>FName LName</h3>
-                <p className="member-role">Member Role</p>
-                <div className="member-social">Social Links</div>
+                <h3>Michael Perez</h3>
+                <p className="member-role">Front-end Engineer</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/Perezented"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/perezenting-michael/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
+                </div>
+                <p className="member-bio">
+                  Michael Perez is from Oklahoma. Previously, Michael worked at
+                  Walgreens as a shift leader and p/t pharmacy technician. Upon
+                  hearing about a great opportunity, Michael joined Lambda
+                  Schools to develop better computer skills. Now Michael is
+                  proficient in Full Stack Web Engineering, honing his skills on
+                  Python, React.js, and Node.js/Express.
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="member-info">
+                <div className="member-pic">
+                  <img
+                    className="member-photo"
+                    alt="default pic"
+                    src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
+                  />
+                </div>
+                <h3>Toricruz Mendiola</h3>
+                <p className="member-role">Front-end Engineer</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/mtoricruz"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/toricruz-mendiola"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
+                </div>
+                <p className="member-bio">
+                  Toricruz Mendiola is a problem-solver enthusiast that is
+                  looking to make a difference with the code he writes. He is
+                  energized by being in environments where his contributions
+                  will create an impact on someone's life. As a software
+                  engineer, he is looking to be a part of organizations that are
+                  making a difference in the world.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="about-us-team-div">
+            <h2 id="data-science" className="team-name">
+              Data Science
+            </h2>
+            <span>Team</span>
+          </div>
+          <div className="card-container">
+            <div className="card">
+              <div className="member-info">
+                <div className="member-pic">
+                  <img
+                    className="member-photo"
+                    alt="default pic"
+                    src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
+                  />
+                </div>
+                <h3>Aaron Watkins</h3>
+                <p className="member-role">Data Science Engineer</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/ekselan"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/aaron-watkins-jr/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
+                </div>
+                <p className="member-bio">
+                  Aaron Watkins is a data scientist with a passion for data and
+                  software engineering, machine learning, and natural language
+                  processing. He is a fantasy sports enthusiast, cinema lover,
+                  UMD alum.
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="member-info">
+                <div className="member-pic">
+                  <img
+                    className="member-photo"
+                    alt="default pic"
+                    src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
+                  />
+                </div>
+                <h3>Samuel Swank</h3>
+                <p className="member-role">Data Science Engineer</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/shengjiyang"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/samuel-swank/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
+                </div>
                 <p className="member-bio">
                   Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
                   Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
@@ -140,15 +258,64 @@ export default function AboutUs() {
                     src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
                   />
                 </div>
-                <h3>FName LName</h3>
-                <p className="member-role">Member Role</p>
-                <div className="member-social">Social Links</div>
+                <h3>Kenneth Barrett</h3>
+                <p className="member-role">Data Science Engineer</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/KennethTBarrett"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/kbarrett96/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
+                </div>
                 <p className="member-bio">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.
+                  Kenneth Barrett is a Data Scientist located in the Houston
+                  Greater Area with a passion for analytics and machine
+                  learning. He is passionate about human progress in science,
+                  technology, and society. Always happy to contribute in helping
+                  others.
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="member-info">
+                <div className="member-pic">
+                  <img
+                    className="member-photo"
+                    alt="default pic"
+                    src="https://i.ibb.co/k3rzB8P/default-prof-pic.jpg"
+                  />
+                </div>
+                <h3>Travis Cain</h3>
+                <p className="member-role">Data Science Engineer</p>
+                <div className="member-social">
+                  <a
+                    href="https://github.com/TravisJRCain"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-github-square fa-3x" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/travisc-/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i class="fab fa-linkedin fa-3x" />
+                  </a>
+                </div>
+                <p className="member-bio">
+                  Data Scientist with a passion for data analytics, data
+                  prediction and sports. Enjoy problem solving and working with
+                  others
                 </p>
               </div>
             </div>
