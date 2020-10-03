a63474bdfad8d42c301c39f9363c18c1d79e0bd1
Michael Perez <mperez6473@gmail.com>
2020-10-03 10:31:46 -0500

    updated city report test, uploading removingCities to work remotely. Still needs work..

--- cypress/integration/Citrics/cityReport.spec.js
+++ cypress/integration/Citrics/cityReport.spec.js
@@ -4,66 +4,68 @@ context('Search bar is able to find cities', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
   });
+  // ONE CITY HERE
 
   it('Selects and views Tulsa, OK', () => {
-    cy.wait(2500);
-    cy.get('.ant-input').click();
-    cy.wait(1500);
+    // await for the search bar info to load
+    cy.wait(3500); // type 'Tulsa, OK' into the search bar
     cy.get('.ant-input')
       .type('Tulsa, OK')
       .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
+    // select on Tulsa from the dropdown
+    cy.contains('Tulsa, OK').click(); // wait for DS API data
+    cy.wait(2000); // make sure 'Tulsa, OK' and the weather card show up
     cy.contains('Tulsa, OK');
     cy.contains('Weather');
   });
+  // TWO CITIES HERE
 
   it('Selects and views two cities', () => {
-    cy.wait(2000);
-    cy.get('.ant-input').click();
-    cy.wait(2000);
+    cy.wait(3500); // await for the search bar info to load
+    // type 'Tulsa, OK' into the search bar
     cy.get('.ant-input')
       .type('Tulsa, OK')
       .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
-    cy.contains('Tulsa, OK');
+    // select on Tulsa from the dropdown
+    cy.contains('Tulsa, OK').click(); // wait for DS API data
+    cy.wait(2000); // make sure 'Tulsa, OK' and the weather card show up
+    cy.contains('Tulsa, OK'); // click on the search bar
     cy.get('.ant-input').click();
-    cy.wait(2500);
+    cy.wait(2500); // type in Seattle, WA
     cy.get('.ant-input')
       .type('Seattle, WA')
-      .should('have.value', 'Seattle, WA');
-    cy.contains('Seattle, WA').click();
-    cy.wait(3000);
+      .should('have.value', 'Seattle, WA'); // Click on Seattle from the dropdown from the search.
+    cy.contains('Seattle, WA').click(); // Wait for data to retrieve from DS API
+    cy.wait(3000); // Seattle, WA should be in the document.
     cy.contains('Seattle, WA');
   });
 
+  // THREE CITIES HERE
   it('Selects and views three cities', () => {
-    cy.wait(2500);
-    cy.get('.ant-input').click();
-    cy.wait(2000);
+    cy.wait(3500); // await for the search bar info to load
+    // type 'Tulsa, OK' into the search bar
     cy.get('.ant-input')
       .type('Tulsa, OK')
       .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
-    cy.contains('Tulsa, OK');
-    cy.wait(2500);
-
+    // select on Tulsa from the dropdown
+    cy.contains('Tulsa, OK').click(); // wait for DS API data
+    cy.wait(2000); // make sure 'Tulsa, OK' and the weather card show up
+    cy.contains('Tulsa, OK'); // click on the search bar
     cy.get('.ant-input').click();
-    cy.wait(2500);
-
+    cy.wait(2500); // type in Seattle, WA
     cy.get('.ant-input')
       .type('Seattle, WA')
-      .should('have.value', 'Seattle, WA');
-    cy.contains('Seattle, WA').click();
+      .should('have.value', 'Seattle, WA'); // Click on Seattle from the dropdown from the search.
+    cy.contains('Seattle, WA').click(); // Wait for data to retrieve from DS API
+    cy.wait(3000); // Seattle, WA & Tulsa, OK should be in the document.
     cy.contains('Tulsa, OK');
-    cy.contains('Seattle, WA');
-    cy.wait(2500);
-
-    cy.get('.ant-input').click();
+    cy.contains('Seattle, WA'); // click on the search bar
+    cy.get('.ant-input').click(); // Type in 'Los Angeles'
     cy.get('.ant-input')
       .type('Los Angeles')
-      .should('have.value', 'Los Angeles');
+      .should('have.value', 'Los Angeles'); // Click on 'Los Angeles from the dropdown on the search bar results
     cy.contains('Los Angeles').click();
-    cy.wait(3000);
+    cy.wait(3000); // wait for data to render, all three cities added should display
     cy.contains('Tulsa, OK');
     cy.contains('Seattle, WA');
     cy.contains('Los Angeles, CA');
--- cypress/integration/Citrics/removingCities.spec.js
+++ cypress/integration/Citrics/removingCities.spec.js
@@ -1,152 +1,74 @@
 /// <reference types="cypress" />
 
-context('Removes cities from comparison list', () => {
+context('Search bar is able to find cities', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
   });
 
-  //    1 city removal
-  it('Selects and views a city, then removes it.', () => {
-    cy.wait(2000);
-    cy.get('.ant-input').click();
+  // ONE CITY HERE
+
+  it('Selects and views Tulsa, OK', () => {
+    // await for the search bar info to load
+    cy.wait(3500); // type 'Tulsa, OK' into the search bar
     cy.get('.ant-input')
       .type('Tulsa, OK')
       .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
+    // select on Tulsa from the dropdown
+    cy.contains('Tulsa, OK').click(); // wait for DS API data
+    cy.wait(2000); // make sure 'Tulsa, OK' and the weather card show up
     cy.contains('Tulsa, OK');
     cy.contains('Weather');
-    cy.get('.remove-btn').click();
-    cy.contains('Visualize Comparisons');
   });
 
-  //    2 city removal
-  it('Selects and views two cities, removes first city', () => {
-    cy.wait(1000);
+  // TWO CITIES HERE
+
+  it('Selects and views two cities', () => {
+    cy.wait(3500); // await for the search bar info to load
+    // type 'Tulsa, OK' into the search bar
+    cy.get('.ant-input')
+      .type('Tulsa, OK')
+      .should('have.value', 'Tulsa, OK');
+    // select on Tulsa from the dropdown
+    cy.contains('Tulsa, OK').click(); // wait for DS API data
+    cy.wait(2000); // make sure 'Tulsa, OK' and the weather card show up
+    cy.contains('Tulsa, OK'); // click on the search bar
     cy.get('.ant-input').click();
-    cy.wait(3000);
+    cy.wait(2500); // type in Seattle, WA
     cy.get('.ant-input')
-      .type('Tulsa, OK')
-      .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
-    cy.contains('Tulsa, OK');
-
-    cy.get('.ant-input')
-      .click()
       .type('Seattle, WA')
-      .should('have.value', 'Seattle, WA');
-    cy.contains('Seattle, WA').click();
-    cy.contains('Tulsa, OK');
+      .should('have.value', 'Seattle, WA'); // Click on Seattle from the dropdown from the search.
+    cy.contains('Seattle, WA').click(); // Wait for data to retrieve from DS API
+    cy.wait(3000); // Seattle, WA should be in the document.
     cy.contains('Seattle, WA');
-    cy.wait(2500);
   });
 
-  it('Selects and views two cities, removes second city', () => {
-    cy.wait(2000);
+  // THREE CITIES HERE
+
+  it('Selects and views three cities', () => {
+    cy.wait(3500); // await for the search bar info to load
+    // type 'Tulsa, OK' into the search bar
     cy.get('.ant-input')
-      .click()
       .type('Tulsa, OK')
       .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
-    cy.contains('Tulsa, OK');
-    cy.wait(2500);
-
+    // select on Tulsa from the dropdown
+    cy.contains('Tulsa, OK').click(); // wait for DS API data
+    cy.wait(2000); // make sure 'Tulsa, OK' and the weather card show up
+    cy.contains('Tulsa, OK'); // click on the search bar
+    cy.get('.ant-input').click();
+    cy.wait(2500); // type in Seattle, WA
     cy.get('.ant-input')
-      .click()
       .type('Seattle, WA')
-      .should('have.value', 'Seattle, WA');
-    cy.contains('Seattle, WA').click();
+      .should('have.value', 'Seattle, WA'); // Click on Seattle from the dropdown from the search.
+    cy.contains('Seattle, WA').click(); // Wait for data to retrieve from DS API
+    cy.wait(3000); // Seattle, WA & Tulsa, OK should be in the document.
     cy.contains('Tulsa, OK');
-    cy.contains('Seattle, WA');
-    cy.wait(2500);
-  });
-
-  //    3 city removal
-
-  it('Selects and views three cities, removes first city', () => {
-    cy.wait(2000);
+    cy.contains('Seattle, WA'); // click on the search bar
+    cy.get('.ant-input').click(); // Type in 'Los Angeles'
     cy.get('.ant-input')
-      .click()
-      .type('Tulsa, OK')
-      .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
-    cy.contains('Tulsa, OK');
-    cy.wait(2500);
-
-    cy.get('.ant-input')
-      .click()
-      .type('Seattle, WA')
-      .should('have.value', 'Seattle, WA');
-    cy.contains('Seattle, WA').click();
-    cy.contains('Tulsa, OK');
-    cy.contains('Seattle, WA');
-    cy.wait(2500);
-
-    cy.get('.ant-input')
-      .click()
       .type('Los Angeles')
-      .should('have.value', 'Los Angeles');
+      .should('have.value', 'Los Angeles'); // Click on 'Los Angeles from the dropdown on the search bar results
     cy.contains('Los Angeles').click();
-    cy.wait(3000);
-    cy.contains('Tulsa, OK');
-    cy.contains('Seattle, WA');
-    cy.contains('Los Angeles, CA');
-  });
-
-  it('Selects and views three cities, removes second city', () => {
-    cy.wait(2000);
-    cy.get('.ant-input')
-      .click()
-      .type('Tulsa, OK')
-      .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
-    cy.contains('Tulsa, OK');
-    cy.wait(2500);
-
-    cy.get('.ant-input')
-      .click()
-      .type('Seattle, WA')
-      .should('have.value', 'Seattle, WA');
-    cy.contains('Seattle, WA').click();
-    cy.contains('Tulsa, OK');
-    cy.contains('Seattle, WA');
-    cy.wait(2500);
-
-    cy.get('.ant-input')
-      .click()
-      .type('Los Angeles')
-      .should('have.value', 'Los Angeles');
-    cy.contains('Los Angeles').click();
-    cy.wait(3000);
-    cy.contains('Tulsa, OK');
-    cy.contains('Seattle, WA');
-    cy.contains('Los Angeles, CA');
-  });
-
-  it('Selects and views three cities, removes third city', () => {
-    cy.wait(2000);
-    cy.get('.ant-input')
-      .click()
-      .type('Tulsa, OK')
-      .should('have.value', 'Tulsa, OK');
-    cy.contains('Tulsa, OK').click();
-    cy.contains('Tulsa, OK');
-    cy.wait(2500);
-
-    cy.get('.ant-input')
-      .click()
-      .type('Seattle, WA')
-      .should('have.value', 'Seattle, WA');
-    cy.contains('Seattle, WA').click();
-    cy.contains('Tulsa, OK');
-    cy.contains('Seattle, WA');
-    cy.wait(2500);
-
-    cy.get('.ant-input')
-      .click()
-      .type('Los Angeles')
-      .should('have.value', 'Los Angeles');
-    cy.contains('Los Angeles').click();
-    cy.wait(3000);
+    cy.wait(3000); // wait for data to render, all three cities added should display
     cy.contains('Tulsa, OK');
     cy.contains('Seattle, WA');
     cy.contains('Los Angeles, CA');
