44d7bb0ff8082a1c29973d20139201d211337993
Toricruz Mendiola <mtoricruz@gmail.com>
2020-10-20 13:28:42 -0700

    added population stat, styled 3 cities to 1 transformation, and added population state to hidecity functionality

--- src/components/pages/CompareCities/PlotlyHelpers/RentPlot.js
+++ src/components/pages/CompareCities/PlotlyHelpers/RentPlot.js
@@ -12,7 +12,6 @@ export default function RentPlot(props) {
   let lastState = props.rentPlotOptions.lastState;
   let lastCityAdded = props.rentPlotOptions.lastCityAdded;
   let setThisCityData = props.rentPlotOptions.setThisCityData;
-  console.log(props);
   // useEffect for fetching ---RENT--- data viz from ds backend
   // sets the cityData and cityLayout for following cities into thisCityData
   useEffect(() => {
@@ -31,8 +30,9 @@ export default function RentPlot(props) {
         });
       } else if (compareList.cities.length === 2) {
         let firstCity = compareList.cities[compareList.cities.length - 2];
+        console.log('first city', firstCity);
         const request = await axios.get(
-          `/rent_viz/${firstCity[0]}_${firstCity[1]}?cityWeather2=${lastCityAdded[0]}&statecode2=${lastCityAdded[1]}`
+          `/rent_viz/${firstCity[0]}_${firstCity[1]}?city2=${lastCityAdded[0]}&statecode2=${lastCityAdded[1]}`
         );
         const rentData = JSON.parse(request.data);
         setThisCityData({
@@ -43,7 +43,7 @@ export default function RentPlot(props) {
         let firstCity = compareList.cities[compareList.cities.length - 3];
         let secondCity = compareList.cities[compareList.cities.length - 2];
         const request = await axios.get(
-          `/rent_viz/${firstCity[0]}_${firstCity[1]}?cityWeather2=${secondCity[0]}&statecode2=${secondCity[1]}&cityWeather3=${lastCityAdded[0]}&statecode3=${lastCityAdded[1]}`
+          `/rent_viz/${firstCity[0]}_${firstCity[1]}?city2=${secondCity[0]}&statecode2=${secondCity[1]}&city3=${lastCityAdded[0]}&statecode3=${lastCityAdded[1]}`
         );
         const rentData = JSON.parse(request.data);
         setThisCityData({
